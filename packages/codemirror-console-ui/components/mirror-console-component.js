63d19e259c046f99ecd7af53a7748fbb2b7b1eb3
azu <azuciao@gmail.com>
2014-06-11 17:19:46 +0900

    feat(cosnole): add toggle console <-> normalview

--- packages/codemirror-console-ui/components/mirror-console-component.js
+++ packages/codemirror-console-ui/components/mirror-console-component.js
@@ -1,35 +1,46 @@
 "use strict";
 var MirrorConsole = require("codemirror-console");
-function createConsole() {
-    var template = require("./mirror-console-component.hbs");
+function getDOMFromTemplate(template) {
     var div = document.createElement("div");
     div.innerHTML = template();
     return div;
 }
-module.exports = function (element) {
+function intendMirrorConsole(element) {
     var mirror = new MirrorConsole;
     var codeMirror = mirror.editor;
     codeMirror.setOption("lineNumbers", true);
     mirror.setText(element.textContent);
-    var node = createConsole();
+    var node = getDOMFromTemplate(require("./mirror-console-component.hbs"));
+    var logArea = node.querySelector(".mirror-console-log");
     var consoleMock = {
         log: function () {
             var args = Array.prototype.slice.call(arguments);
-            var logArea = node.querySelector(".mirror-console-log");
             var div = document.createElement("div");
-            div.className = "mirror-console-log-row"
+            div.className = "mirror-console-log-row mirror-console-log-normal";
+            div.appendChild(document.createTextNode(args.join(",")));
+            logArea.appendChild(div);
+        },
+        error: function () {
+            var args = Array.prototype.slice.call(arguments);
+            var div = document.createElement("div");
+            div.className = "mirror-console-log-row mirror-console-log-error";
             div.appendChild(document.createTextNode(args.join(",")));
             logArea.appendChild(div);
         }
     }
     mirror.swapWithElement(element);
     mirror.textareaHolder.appendChild(node);
-    node.querySelector("#mirror-console-run-button").addEventListener("click", function runJS() {
+    runCode();
+    function runCode() {
         mirror.runInContext({ console: consoleMock }, function (error) {
             if (error) {
-                console.error(error);
+                consoleMock.error(error);
             }
         });
+    }
+
+    node.querySelector("#mirror-console-run-button").addEventListener("click", function runJS() {
+        runCode();
     });
     node.querySelector("#mirror-console-clear-button").addEventListener("click", function clearLog() {
         var range = document.createRange();
@@ -38,5 +49,22 @@ module.exports = function (element) {
     });
     node.querySelector("#mirror-console-exit-button").addEventListener("click", function clearLog() {
         mirror.destroy();
+        firstTouch(element);
     });
+}
+function firstTouch(element) {
+    var parentNode = element.parentNode;
+    var node = getDOMFromTemplate(require("./mirror-console-inject-div.hbs"));
+    node.querySelector("#mirror-console-run-button").addEventListener("click", function editAndRun() {
+        intendMirrorConsole(element);
+        parentNode.removeChild(node);
+    });
+    if (element.nextSibling === null) {
+        parentNode.appendChild(node);
+    } else {
+        parentNode.insertBefore(node, element.nextSibling);
+    }
+}
+module.exports = function (element) {
+    firstTouch(element);
 }
